cba7082f5998103bcf60f7ef46dc819bb4b5f47b
Ibragim Gordanov <gordanov@global.itsph.ru>
2018-06-17 01:39:49 +0300

    tmp

--- script/PS2ControllerDriver.py
+++ script/PS2ControllerDriver.py
@@ -5,22 +5,22 @@ from pynput.keyboard import Key, Controller
 
 keyboard = Controller()
 
-delta_time = 0.1
+delta_time = 0.05
 
 ser = serial.Serial('COM3', 57600, timeout=0)
 
 keys = [
-    ['0',     False,  0.0],     #squire
-    ['1',     False,  0.0],     #x
-    ['2',     False,  0.0],     #down
-    ['3',     False,  0.0],     #left
-    ['4',     False,  0.0],     #right
-    ['5',     False,  0.0],     #up
-    ['6',     False,  0.0],     #select
-    ['7',     False,  0.0],     #start
-    ['8',     False,  0.0],     #start
-    ['9',     False,  0.0],     #start
-    # 1 - left V
+    ['0',     False,  0.0],     #select 
+    ['1',     False,  0.0],     #start 
+    ['2',     False,  0.0],     #squire 
+    ['3',     False,  0.0],     #cross 
+    ['4',     False,  0.0],     #down 
+    ['5',     False,  0.0],     #left 
+    ['6',     False,  0.0],     #right
+    ['7',     False,  0.0],     #up
+    #['8',     False,  0.0],     #start
+    #['9',     False,  0.0],     #start
+    # 1 - left V 
     # 2 - left H
     # 3 - right V
     # 4 - right H775
@@ -30,10 +30,9 @@ while 1:
     data  = ser.read()
     if data:   
         keys_state = ord(data)
-        #print("{0:b}".format(keys_state))
-        for i in range(10):
+        for i in range(8):	
             key = keys[i]
-            if not bool(keys_state & (1 << i)) != key[1] and time.time() - key[2] > delta_time:
+            if bool(keys_state & (1 << i)) == key[1] and time.time() - key[2] > delta_time:
                 print(key)
                 if not key[1]:
                     keyboard.release(key[0])
